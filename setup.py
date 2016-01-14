12c94e4c89a6defb62b2862a78eaec912791f72a
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-14 00:33:59 -0600

    fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(name='dstools',
-      packages=['dstools'],
-      version='0.1',
+setup(name = 'dstools',
+      packages = ['dstools'],
+      version = '0.2',
       description='Utility functions for Data Science projects.',
       url='https://github.com/edublancas/dstools',
-      download_url = 'https://github.com/edublancas/dstools/tarball/0.1'
-      author='Eduardo Blancas Reyes',
-      author_email='edu.blancas@gmail.com',
-      license='MIT',
+      download_url = 'https://github.com/edublancas/dstools/tarball/0.2',
+      author = 'Eduardo Blancas Reyes',
+      author_email = 'edu.blancas@gmail.com',
+      license ='MIT',
+      keywords = ['datascience'],
+      classifiers = [],
 )
