2cbc24cd0b81a7bde4937bc572935df71808e317
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-14 00:20:52 -0600

    updates setup

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ setup(name='dstools',
       packages=['dstools'],
       version='0.1',
       description='Utility functions for Data Science projects.',
-      url='https://github.com/edublancas/python-ds-tools',
-      download_url = 'https://github.com/edublancas/mypackage/tarball/0.1'
+      url='https://github.com/edublancas/dstools',
+      download_url = 'https://github.com/edublancas/dstools/tarball/0.1'
       author='Eduardo Blancas Reyes',
       author_email='edu.blancas@gmail.com',
       license='MIT',
-      #test_suite='nose.collector',
-      #tests_require=['nose'],
-      #zip_safe=False
 )
