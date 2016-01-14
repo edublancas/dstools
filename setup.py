50cfebc8ddc9a222800e08f08860643923931f3d
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-14 00:40:32 -0600

    adds reqs to setup

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup
 
 setup(name = 'dstools',
       packages = ['dstools'],
-      version = '0.2',
+      version = '0.3',
       description='Utility functions for Data Science projects.',
       url='https://github.com/edublancas/dstools',
-      download_url = 'https://github.com/edublancas/dstools/tarball/0.2',
+      download_url = 'https://github.com/edublancas/dstools/tarball/0.3',
       author = 'Eduardo Blancas Reyes',
       author_email = 'edu.blancas@gmail.com',
       license ='MIT',
       keywords = ['datascience'],
       classifiers = [],
+      install_requires=[
+          'pyaml',
+      ]
 )
