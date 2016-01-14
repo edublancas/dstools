818b20880b96726c71292c805c6d457ebefe393b
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-01-14 00:19:29 -0600

    removes underscore

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='ds-tools',
-      packages=['ds_tools'],
+setup(name='dstools',
+      packages=['dstools'],
       version='0.1',
       description='Utility functions for Data Science projects.',
       url='https://github.com/edublancas/python-ds-tools',
+      download_url = 'https://github.com/edublancas/mypackage/tarball/0.1'
       author='Eduardo Blancas Reyes',
       author_email='edu.blancas@gmail.com',
       license='MIT',
