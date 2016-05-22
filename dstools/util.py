abc58fff40bc249ada722a585745e2680f7027f1
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-05-22 14:31:43 -0500

    improves code structure

--- dstools/util.py
+++ dstools/util.py
@@ -1,3 +1,7 @@
+import os
+import yaml
+
+
 def _can_iterate(obj):
     import types
     import collections
@@ -11,8 +15,28 @@ def _can_iterate(obj):
 def format_column_names(columns, prefix=None):
     import re
     import pandas as pd
+
     # Get rid of non alphanumeric characters and capital letters
-    f = lambda s: re.sub('[^0-9a-zA-Z]+', '_', s).lower()
-    names = columns.map(f)
-    names = pd.Series(names).map(lambda s: '{}_{}'.format(prefix, s)) if prefix else names
+    def format_str(s):
+        re.sub('[^0-9a-zA-Z]+', '_', s).lower()
+
+    names = columns.map(format_str)
+
+    if prefix:
+        names = pd.Series(names).map(lambda s: '{}_{}'.format(prefix, s))
+
     return names
+
+
+def load_yaml(name):
+    folder = os.environ['ROOT_FOLDER']
+    path = "%s/%s" % (folder, name)
+    with open(path, 'r') as f:
+        text = f.read()
+    dic = yaml.load(text)
+    return dic
+
+config = load_yaml('config.yaml')
+
+pg_uri = ('{dialect}://{user}:{password}@{host}:5432/{database}'
+          .format(**config['db']))
