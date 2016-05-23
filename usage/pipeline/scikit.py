21f116a7a319089f2f0f24e2d5ad9f96ef57a361
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-05-23 12:56:27 -0500

    improves scikit-exampl

--- usage/pipeline/scikit.py
+++ usage/pipeline/scikit.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn import cross_validation
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.pipeline import make_pipeline
 from itertools import product
 import logging
 
@@ -35,10 +36,6 @@ def load(config):
     data['train_y'] = train.survived.values
     test = pd.read_csv('../data/test.csv', index_col='id')
     data['test_x'] = test.values
-    # scale data
-    scaler = StandardScaler().fit(data['train_x'])
-    data['train_x'] = scaler.transform(data['train_x'])
-    data['test_x'] = scaler.transform(data['test_x'])
     data['test_ids'] = test.index
     return data
 
@@ -61,23 +58,33 @@ def model_gen(config, models):
 def train(config, model, data, record):
     model, percentile = model
 
+    record['model'] = model_name(model)
+    record['parameters'] = model.get_params()
+    record['feats_percentile'] = percentile
+
     train_x = data['train_x']
     train_y = data['train_y']
     test_x = data['test_x']
 
+    # estimate accuracy using cross-validation
+    model = make_pipeline(SelectPercentile(f_classif, percentile),
+                          StandardScaler(), model)
+
+    scores = cross_validation.cross_val_score(model, train_x,
+                                              train_y, cv=5,
+                                              scoring='accuracy')
+    record['mean_acc'] = scores.mean()
+
+    # predict on the test set
     fn = SelectPercentile(f_classif, percentile).fit(train_x, train_y)
     train_x = fn.transform(train_x)
     test_x = fn.transform(test_x)
 
-    scores = cross_validation.cross_val_score(model, train_x,
-                                              train_y, cv=3,
-                                              scoring='accuracy')
-    model.fit(train_x, train_y)
-    record['parameters'] = model.get_params()
-    record['model'] = model_name(model)
-    record['mean_acc'] = scores.mean()
-    record['feats_percentile'] = percentile
+    scaler = StandardScaler().fit(train_x)
+    train_x = scaler.transform(train_x)
+    test_x = scaler.transform(test_x)
 
+    model.fit(train_x, train_y)
     ids = data['test_ids']
     preds = model.predict(test_x)
     record['test_preds'] = [(id_, pred) for id_, pred in zip(ids, preds)]
@@ -89,8 +96,8 @@ def post_train(config, models, data, record):
 
 def finalize(config, models, experiment):
     experiment.records = top_k(experiment.records, 'mean_acc', 10)
-    experiment['exp_name'] = 'testing'
-    # experiment.save()
+    experiment['exp_name'] = 'fixed-transform-2'
+    experiment.save()
 
 pip = Pipeline(config, load_yaml('exp.yaml'))
 
