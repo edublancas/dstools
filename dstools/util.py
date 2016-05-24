eca1bf15bf8feb87cf6c0b0ce215ed855b94db2d
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-05-24 16:12:41 -0500

    adds function to hash numpy arrays

--- dstools/util.py
+++ dstools/util.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import hashlib
 
 
 def instantiate_from_class_string(class_str, kwargs):
@@ -49,3 +50,20 @@ def load_yaml(path):
         text = f.read()
 
     return yaml.load(text)
+
+
+# based on:
+#   http://stackoverflow.com/questions/5386694/fast-way-to-hash-numpy-objects-for-caching
+#   http://stackoverflow.com/questions/806151/how-to-hash-a-large-object-dataset-in-python
+def hash_numpy_array(a):
+    '''
+        Hash a numpy array using sha1.
+    '''
+    import numpy as np
+    # conver to contigous in case the array has a different
+    # representation
+    a = np.ascontiguousarray(a)
+    # get a view from the array, this will help produce different hashes
+    # for arrays with same data but different shapes
+    a = a.view(np.uint8)
+    return hashlib.sha1(a).hexdigest()
