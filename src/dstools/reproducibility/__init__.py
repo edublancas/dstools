95ed796625360cf33bf6bee7c2a16756090a6053
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-19 14:58:07 -0400

    imports

--- src/dstools/reproducibility/__init__.py
+++ src/dstools/reproducibility/__init__.py
@@ -1,6 +1,6 @@
-from dstools.reproducibility.util import (make_path, make_filename, hash_array,
+from dstools.reproducibility.util import (make_filename, hash_array,
                                           make_logger_file,
                                           setup_logger)
 
-__all__ = ['make_path', 'make_filename', 'hash_array', 'make_logger_file',
+__all__ = ['make_filename', 'hash_array', 'make_logger_file',
            'setup_logger']
