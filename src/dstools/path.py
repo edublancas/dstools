2da91f7ab991f07c5e5bc2464c60a4d652cc1d09
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-03-07 19:14:54 -0500

    adds _path_to_origin

--- src/dstools/path.py
+++ src/dstools/path.py
@@ -6,8 +6,9 @@ class PathManager:
     def __init__(self, path_to_env, env):
         self.home = Path(path_to_env).resolve().parent
 
-        path_to_output = env._env_content.get('path_to_output')
         subdirectory = env.name if env.name != 'root' else ''
+
+        path_to_output = env._env_content.get('_path_to_output')
 
         # if there is no path_to_output key, use default location
         if path_to_output is None:
@@ -23,3 +24,20 @@ class PathManager:
 
         if not self.output.is_dir():
             self.output.mkdir()
+
+        path_to_origin = env._env_content.get('_path_to_origin')
+
+        # if there is no path_to_origin key, use default location
+        if path_to_origin is None:
+            self.origin = self.home / 'origin' / subdirectory
+
+        else:
+            path_to_origin = Path(path_to_origin).expanduser()
+
+            if not path_to_origin.is_absolute():
+                raise ValueError('_path_to_origin must be an absolute path')
+
+            self.origin = path_to_origin
+
+        if not self.origin.is_dir():
+            self.origin.mkdir()
