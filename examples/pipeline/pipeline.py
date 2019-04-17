f97c34fa7d9a280287242bfb8135b9b917d158cb
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-04-16 22:04:19 -0400

    ability to set upstream dags

--- examples/pipeline/pipeline.py
+++ examples/pipeline/pipeline.py
@@ -61,7 +61,6 @@ sample_task.set_upstream(get_data_task)
 # should perform some basic checking on the output: like, number of rows
 # proportion of NAs. possibly stored historic results to compare how these
 # checks have changed across versions
-
 red_task = BashCommand('csvsql --db {db} --tables red --insert {path} '
                        '--overwrite',
                        pg.PostgresRelation(('public', 'red', 'table')),
--- src/dstools/pipeline/dag.py
+++ src/dstools/pipeline/dag.py
@@ -6,10 +6,33 @@ import networkx as nx
 # NOTE: requires pygraphviz and pygraphviz
 
 
+class MetaProduct:
+
+    def __init__(self, dag):
+        self.dag = dag
+
+    def outdated(self):
+        return (self.outdated_data_dependencies()
+                or self.outdated_code_dependency())
+
+    def outdated_data_dependencies(self):
+        return any([t.product.outdated_data_dependencies()
+                    for t in self.dag.tasks])
+
+    def outdated_code_dependency(self):
+        return any([t.product.outdated_code_dependency()
+                    for t in self.dag.tasks])
+
+    @property
+    def timestamp(self):
+        return max([t.product.timestamp for t in self.dag.tasks])
+
+
 class DAG:
 
     def __init__(self):
         self.tasks = []
+        self.product = MetaProduct(self)
 
     def add_task(self, task):
         self.tasks.append(task)
