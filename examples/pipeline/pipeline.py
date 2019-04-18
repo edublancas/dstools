eb29781685df3859e5952e3218819ce21d114484
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-04-18 01:53:10 -0400

    removed todos

--- examples/pipeline/pipeline.py
+++ examples/pipeline/pipeline.py
@@ -36,7 +36,6 @@ def close_conn():
 
 dag = DAG()
 
-# TODO: be able to specify more than one product?
 get_data_task = BashScript(home / 'get_data.sh',
                            File(env.path.input / 'raw' / 'red.csv'),
                            dag)
@@ -46,24 +45,6 @@ red_path = env.path.input / 'sample' / 'red.csv'
 sample_task = PythonScript(home / 'sample.py', File(red_path), dag)
 sample_task.set_upstream(get_data_task)
 
-# TODO: also have to rollback automatically when something goes wrong
-# when runinng SQL code
-# TODO: double check that the Product is matches what is expected in the
-# code using regex
-# TODO: convention over configuration, we can infer most parameters if we infer
-# them, for example, if sql files follow a naming convention such as
-# create_[kind]_[schema].[name], we can infer identifier, to we only
-# need to pass the path to the file
-# TODO: write motivation in the readme file
-# TODO: migrate this pipeline to the ds-template project
-# TODO: add products as edge names in plot
-# TODO: consider adding a new type of task named Check, this should be run
-# every time its upstream task is run (no need to check dependencies) and
-# should perform some basic checking on the output: like, number of rows
-# proportion of NAs. possibly stored historic results to compare how these
-# checks have changed across versions
-# TODO: product.blame() should return metadata, who modified the product,
-# when (human-readable format), and git metadata
 red_task = BashCommand('csvsql --db {db} --tables red --insert {path} '
                        '--overwrite',
                        pg.PostgresRelation(('public', 'red', 'table')),
