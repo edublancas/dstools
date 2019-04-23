cdf02c10048e718c9a2d42beebe74716e8563eb4
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-04-22 22:15:34 -0400

    adds missing click dependency

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     ],
     install_requires=[
         # FIXME: remove pyyaml
-        'pyyaml', 'networkx'
+        'pyyaml', 'networkx', 'click'
     ],
     extras_require={
         # eg:
