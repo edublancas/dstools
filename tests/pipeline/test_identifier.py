b8dc1c3bd6e99e22c7f09aa0d8651b041647d4df
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-06-26 18:26:17 -0400

    comments out unused tests

--- tests/pipeline/test_identifier.py
+++ tests/pipeline/test_identifier.py
@@ -3,11 +3,11 @@ from dstools.pipeline.identifiers import StringIdentifier
 from jinja2 import Template
 
 
-f = File('/path/to/file')
+# f = File('/path/to/file')
 
-str(f)
+# str(f)
 
-f_t = File(Template('/path/to/file'))
+# f_t = File(Template('/path/to/file'))
 
 
-str(f_t)
+# str(f_t)
