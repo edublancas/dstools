3363b55c4235680a6318683f5e0bd87a8f229e42
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-07-17 11:47:05 -0400

    adds test

--- tests/templates/test_strict_template.py
+++ tests/templates/test_strict_template.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 import pytest
 from dstools.templates.StrictTemplate import StrictTemplate
+from dstools.templates import SQLStore
 
 
 def test_raises_error_if_missing_parameter():
@@ -11,3 +14,17 @@ def test_raises_error_if_extra_parameter():
     with pytest.raises(TypeError):
         (StrictTemplate('SELECT * FROM {{table}}')
          .render(table=1, not_a_param=1))
+
+
+def test_can_create_template_loaded_from_sql_store(tmp_directory):
+    Path(tmp_directory, 'template.sql').write_text('{{file}}')
+
+    store = SQLStore(None, tmp_directory)
+
+    t = store.get_template('template.sql')
+
+    assert t.render({'file': 'some file'})
+
+    tt = StrictTemplate(t)
+
+    assert tt.render({'file': 'some file'})
