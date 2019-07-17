25dcb624f59740f63cd66e82212eb091b651a0b5
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-07-16 22:05:55 -0400

    cleans up test

--- tests/templates/test_strict_template.py
+++ tests/templates/test_strict_template.py
@@ -1,18 +1,13 @@
 import pytest
 from dstools.templates.StrictTemplate import StrictTemplate
 
-t = StrictTemplate("""
-
-SELECT * FROM {{table}}
-
-""")
-
 
 def test_raises_error_if_missing_parameter():
     with pytest.raises(TypeError):
-        t.render()
+        StrictTemplate('SELECT * FROM {{table}}').render()
 
 
 def test_raises_error_if_extra_parameter():
     with pytest.raises(TypeError):
-        t.render(table=1, not_a_param=1)
+        (StrictTemplate('SELECT * FROM {{table}}')
+         .render(table=1, not_a_param=1))
