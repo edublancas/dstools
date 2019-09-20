3331502818fb7ff2663fd0db3af5c7ccda6bd0e3
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-09-20 17:04:05 -0400

    adds two new clients, one for executing scripts in a local shell and another one for doing it over ssh [WIP]

--- src/dstools/pipeline/clients.py
+++ src/dstools/pipeline/clients.py
@@ -2,10 +2,19 @@
 A client reflects a connection to a system that performs the actual
 computations
 """
+import logging
 import atexit
+import shlex
+import subprocess
+from subprocess import CalledProcessError
+
+from dstools.templates.StrictTemplate import StrictTemplate
+
 from sqlalchemy import create_engine
 
 ENGINES = []
+
+# TODO: make all clients expose the same API
 
 
 class Client:
@@ -14,6 +23,8 @@ class Client:
 
     def __init__(self, uri):
         self._uri = uri
+        self._logger = logging.getLogger('{}.{}'.format(__name__,
+                                                        type(self).__name__))
 
     def connect(self):
         raise NotImplementedError("This method must be implemented in the "
@@ -23,8 +34,15 @@ class Client:
     def uri(self):
         return self._uri
 
+    def run(self, code):
+        """Run code
+        """
+        raise NotImplementedError("This method must be implemented in the "
+                                  "subclasses")
+
 
 class SQLAlchemyClient(Client):
+
     def __init__(self, uri):
 
         super().__init__(uri)
@@ -69,6 +87,50 @@ class SQLAlchemyClient(Client):
         if self._engine is not None:
             print(f'Disposing engine {self._engine}')
             self._engine.dispose()
+
+
+class ShellClient(Client):
+    """Client to run command in the local shell
+    """
+
+    def __init__(self, run_template='bash {{path_to_code}}'):
+        """
+        """
+        self.run_template = StrictTemplate(run_template)
+        self._logger = logging.getLogger('{}.{}'.format(__name__,
+                                                        type(self).__name__))
+
+    def run(self, code):
+        """Run code
+        """
+        kwargs = {'stderr': subprocess.PIPE,
+                  'stdout': subprocess.PIPE,
+                  'shell': True}
+
+        path_to_code = code.save_to_tmp_file()
+        source = self.run_template.render(dict(path_to_code=path_to_code))
+
+        res = subprocess.run(shlex.split(source))
+
+        if res.returncode != 0:
+            # log source code without expanded params
+            self._logger.info(f'{self.source_code} returned stdout: '
+                              f'{res.stdout} and stderr: {res.stderr} '
+                              f'and exit status {res.returncode}')
+            raise CalledProcessError(res.returncode, self.source_code)
+        else:
+            self._logger.info(f'Finished running {self}. stdout: {res.stdout},'
+                              f' stderr: {res.stderr}')
+
+
+class RemoteShellClient(Client):
+    """Client to run commands in a remote shell
+    """
+
+    def run(self, code):
+        """Run code
+        """
+        pass
 
 
 @atexit.register
