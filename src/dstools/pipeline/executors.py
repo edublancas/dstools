97c52bb9a7ba6766fd9c738726ec4a2c405dd216
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-10-15 18:52:58 -0400

    moves parallel execution to executors module

--- src/dstools/pipeline/executors.py
+++ src/dstools/pipeline/executors.py
@@ -1,9 +1,12 @@
 """
 DAG executors
 """
+from multiprocessing import Pool, TimeoutError
+
 import networkx as nx
 from tqdm.auto import tqdm
 from dstools.pipeline.Table import BuildReport
+from dstools.pipeline.tasks.TaskStatus import TaskStatus
 
 
 def serial(dag):
@@ -31,9 +34,73 @@ def serial(dag):
         status_all.append(t.build_report)
 
     build_report = BuildReport(status_all)
-    dag._logger.info(' DAG report:\n{}'.format(repr(dag.build_report)))
+    dag._logger.info(' DAG report:\n{}'.format(repr(build_report)))
 
     for client in dag.clients.values():
         client.close()
 
     return build_report
+
+
+def parallel(dag):
+    """Runs a DAG in parallel using the multiprocessing module
+    """
+    # TODO: Have to test this with other Tasks, especially the ones that use
+    # clients - have to make sure they are serialized correctly
+    done = []
+    started = []
+
+    def callback(task):
+        """Keep track of finished tasks
+        """
+        done.append(task)
+
+    def next_task():
+        """
+        Return the next Task to execute, returns None if no Tasks are available
+        for execution (cause their dependencies are not done yet) and raises
+        a StopIteration exception if there are no more tasks to run, which means
+        the DAG is done
+        """
+        if done:
+            for task in done:
+                task = dag[task.name]
+                # TODO: must check for esxecution status - if there is an error
+                # is this status updated automatically? cause it will be better
+                # for tasks to update their status by themselves, then have a
+                # manager to update the other tasks statuses whenever one finishes
+                # to know which ones are available for execution
+                task._status = TaskStatus.Executed
+
+                # update other tasks status, should abstract this in a execution
+                # manager, also make the _get_downstream more efficient by
+                # using the networkx data structure directly
+                for t in task._get_downstream():
+                    t._update_status()
+
+        # iterate over tasks to find which is ready for execution
+        for task_name in dag:
+            # ignore tasks that are already started, I should probably add an
+            # executing status but that cannot exist in the task itself,
+            # maybe in the manaer?
+            if (dag[task_name]._status == TaskStatus.WaitingExecution
+                    and dag[task_name] not in started):
+                t = dag[task_name]
+                return t
+
+        # if all tasks are done, stop
+        if set([t.name for t in done]) == set(dag):
+            raise StopIteration
+
+    with Pool(processes=4) as pool:
+        while True:
+            try:
+                task = next_task()
+            except StopIteration:
+                break
+            else:
+                if task is not None:
+                    res = pool.apply_async(task.build, [], callback=callback)
+                    started.append(task)
+                    print('started', task.name)
+                    # time.sleep(3)
--- tests/pipeline/test_executor_parallel.py
+++ tests/pipeline/test_executor_parallel.py
@@ -27,3 +27,45 @@ def test_parallel_execution():
 
     for n, t in dag._dict.items():
         print(n, t, t._status)
+
+import time
+from pathlib import Path
+
+
+from dstools.exceptions import RenderError
+from dstools.pipeline import DAG
+from dstools.pipeline.products import File, PostgresRelation
+from dstools.pipeline.tasks import PythonCallable, SQLScript, BashCommand
+
+
+def fna1(product):
+    print('running fna1')
+    Path(str(product)).touch()
+    time.sleep(3)
+
+
+def fna2(product):
+    print('running fna2')
+    time.sleep(3)
+    Path(str(product)).touch()
+
+
+def fnb(upstream, product):
+    Path(str(product)).touch()
+
+
+def fnc(upstream, product):
+    Path(str(product)).touch()
+
+
+dag = DAG('dag')
+
+a1 = PythonCallable(fna1, File('a1.txt'), dag, 'a1')
+a2 = PythonCallable(fna2, File('a2.txt'), dag, 'a2')
+b = PythonCallable(fnb, File('b.txt'), dag, 'b')
+c = PythonCallable(fnc, File('c.txt'), dag, 'c')
+
+
+(a1 + a2) >> b >> c
+
+dag.render()
