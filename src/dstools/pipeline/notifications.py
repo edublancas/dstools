5d8d127e80b6a06c3722ac94db157d2195caecde
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-10-15 15:13:32 -0400

    showing warnings if cannot send email

--- src/dstools/pipeline/notifications.py
+++ src/dstools/pipeline/notifications.py
@@ -1,5 +1,6 @@
 from dstools import Env
 
+import warnings
 import smtplib
 from email.message import EmailMessage
 
@@ -10,10 +11,27 @@ def send_email(subject, body):
     msg = EmailMessage()
     msg.set_content(body)
 
-    msg['Subject'] = subject
-    msg['From'] = env.notifications.email.sender
-    msg['To'] = env.notifications.email.to
+    try:
+        sender = env.notifications.email.sender
+    except KeyError:
+        sender = None
 
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
+    try:
+        to = env.notifications.email.to
+    except KeyError:
+        to = None
+
+    if sender is not None and to is not None:
+        msg['Subject'] = subject
+        msg['From'] = sender
+        msg['To'] = env.notifications.email.to
+
+        s = smtplib.SMTP('localhost')
+        s.send_message(msg)
+        s.quit()
+    else:
+        if sender is None:
+            warnings.warn('Cannot send email, could not load sender')
+
+        if to is None:
+            warnings.warn('Cannot send email, could not load recipient')
