d301ded357178f89f3b1fcc80016081b669eb8b0
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-10-29 17:16:04 -0400

    NotebookRunner now can export to html from ipynb

--- src/dstools/pipeline/tasks/notebook.py
+++ src/dstools/pipeline/tasks/notebook.py
@@ -1,3 +1,6 @@
+from tempfile import mktemp
+from pathlib import Path
+
 try:
     import papermill as pm
 except ImportError:
@@ -13,6 +16,12 @@ try:
 except ImportError:
     nbformat = None
 
+try:
+    import nbconvert
+except ImportError:
+    nbconvert = None
+
+
 from dstools.pipeline.placeholders import ClientCodePlaceholder
 from dstools.pipeline.products import File
 from dstools.pipeline.tasks.Task import Task
@@ -21,15 +30,31 @@ from dstools.pipeline.tasks.Task import Task
 def _to_ipynb(source, extension):
     """Convert to jupyter notebook via jupytext
     """
-    nb = jupytext.reads(source, fmt={'extension': '.py'})
+    nb = jupytext.reads(source, fmt={'extension': extension})
 
     # what if the file does not contain kernelspec info?
 
     # tag the first cell as "parameters" for papermill to inject them
     nb.cells[0]['metadata']['tags'] = ["parameters"]
 
+    out = mktemp()
+    Path(out).write_text(nbformat.v4.writes(nb))
 
-    path_to_ipynb.write_text(nbformat.v4.writes(nb))
+    return out
+
+
+def _from_ipynb(path_to_nb, extension):
+    # TODO: support more extensions, also pass custom parameters
+    ext_map = {'.html': nbconvert.HTMLExporter}
+    path = Path(path_to_nb)
+
+    nb = nbformat.v4.reads(path.read_text())
+    content, _ = nbconvert.export(ext_map[extension], nb,
+                                  exclude_input=True)
+
+    path.write_text(content)
+
+    return content
 
 
 class NotebookRunner(Task):
@@ -46,12 +71,19 @@ class NotebookRunner(Task):
         super().__init__(code, product, dag, name, params)
 
     def run(self):
-        # FIXME: do not use this, since it can be templated code,
-        # use rendered version, just save it in a tmp file or file-like
-        # object
-        path_to_in = str(self._code.path)
         path_to_out = str(self.product)
-        # if pointing to a py file, convert to ipynb first using jupytext
+
+        source = str(self._code)
+        ext_in = Path(self._code.path).suffix
+        ext_out = Path(path_to_out).suffix
+
+        # need to convert to ipynb using jupytext
+        if ext_in != '.ipynb':
+            path_to_in = _to_ipynb(source, ext_in)
+        else:
+            # otherwise just save rendered code in a tmp file
+            path_to_in = mktemp()
+            Path(path_to_in).write_text(source)
 
         # papermill only allows JSON serializable parameters
         self.params['product'] = str(self.params['product'])
@@ -61,3 +93,6 @@ class NotebookRunner(Task):
                                 **self.papermill_params)
 
         # if output format other than ipynb, convert using nbconvert
+        # and overwrite
+        if ext_out != '.ipynb':
+            _from_ipynb(path_to_out, ext_out)
--- tests/pipeline/test_notebook_task.py
+++ tests/pipeline/test_notebook_task.py
@@ -5,10 +5,28 @@ from dstools.pipeline.tasks import NotebookRunner
 from dstools.pipeline.products import File
 
 
-def test_can_execute_ipynb(path_to_assets, tmp_directory):
+def test_can_execute_from_ipynb(path_to_assets, tmp_directory):
     dag = DAG()
 
     NotebookRunner(path_to_assets / 'sample.ipynb',
                    product=File(Path(tmp_directory, 'out.ipynb')),
                    dag=dag)
     dag.build()
+
+
+def test_can_execute_to_html(path_to_assets, tmp_directory):
+    dag = DAG()
+
+    NotebookRunner(path_to_assets / 'sample.ipynb',
+                   product=File(Path(tmp_directory, 'out.html')),
+                   dag=dag)
+    dag.build()
+
+
+# def test_can_execute_from_py(path_to_assets, tmp_directory):
+#     dag = DAG()
+
+#     NotebookRunner(path_to_assets / 'sample.py',
+#                    product=File(Path(tmp_directory, 'out.ipynb')),
+#                    dag=dag)
+#     dag.build()
