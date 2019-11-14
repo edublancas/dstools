ce3ad288897ff1731d445f928059ccab180377d4
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-11-13 22:24:48 -0500

    fixes test

--- tests/pipeline/test_CodeDiffer.py
+++ tests/pipeline/test_CodeDiffer.py
@@ -4,18 +4,18 @@ from dstools.pipeline.CodeDiffer import CodeDiffer
 def test_python_differ_ignores_docstrings():
 
     a = '''
-    def x():
-        """This is some docstring
-        """
-        pass
-    '''
+def x():
+    """This is some docstring
+    """
+    pass
+'''
 
     b = '''
-    def x():
-        """This is a docstring and should be ignored
-        """
-        pass
-    '''
+def x():
+    """This is a docstring and should be ignored
+    """
+    pass
+'''
 
     differ = CodeDiffer()
     assert not differ.code_is_different(a, b, language='python')
@@ -24,17 +24,17 @@ def test_python_differ_ignores_docstrings():
 def test_python_differ_ignores_comments():
 
     a = '''
-    def x():
-        # this is a comment
-        # another comment
-        var = 100
-    '''
+def x():
+    # this is a comment
+    # another comment
+    var = 100
+'''
 
     b = '''
-    def x():
-        # one comment
-        var = 100 # this is a comment
-    '''
+def x():
+    # one comment
+    var = 100 # this is a comment
+'''
 
     differ = CodeDiffer()
     assert not differ.code_is_different(a, b, language='python')
