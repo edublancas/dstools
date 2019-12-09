5e752772bb7e362bf3e7de768321e77ea37a4bcd
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-12-09 16:42:45 -0500

    missing imports

--- src/dstools/pipeline/tasks/__init__.py
+++ src/dstools/pipeline/tasks/__init__.py
@@ -1,6 +1,6 @@
 from dstools.pipeline.tasks.tasks import (BashCommand, PythonCallable,
                                           ShellScript, DownloadFromURL,
-                                          Null)
+                                          Null, Link, Input)
 from dstools.pipeline.tasks.Task import Task
 from dstools.pipeline.tasks.TaskFactory import TaskFactory
 from dstools.pipeline.tasks.sql import (SQLScript, SQLDump, SQLTransfer,
@@ -9,4 +9,5 @@ from dstools.pipeline.tasks.notebook import NotebookRunner
 
 __all__ = ['BashCommand', 'PythonCallable', 'ShellScript', 'TaskFactory',
            'Task', 'SQLScript', 'SQLDump', 'SQLTransfer', 'SQLUpload',
-           'PostgresCopy', 'NotebookRunner', 'DownloadFromURL', 'Null']
+           'PostgresCopy', 'NotebookRunner', 'DownloadFromURL', 'Null',
+           'Link', 'Input']
