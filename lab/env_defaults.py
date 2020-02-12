aef2619e2e2451f94f55da2a0fc790e19f1acd9b
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-12 00:12:34 -0500

    env_defaults [WIP]

---
+++ lab/env_defaults.py
@@ -0,0 +1,39 @@
+import collections.abc
+import logging
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+
+# TODO: keep track of nested keys so we can say things like
+# db.postgres.uri will be overwritten
+def update(d, u):
+    """
+    https://stackoverflow.com/a/3233356/709975
+    """
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+defaults = {'a': 1, 'b': 2}
+current = {'z': 1, 'a': 10}
+
+defaults_k = set(defaults)
+current_k = set(current)
+
+logger.info('a')
+
+# loaded from defaults
+defaults_k - current_k
+
+
+# overwritten
+defaults_k & current_k
+
+
+{**defaults, **current}
