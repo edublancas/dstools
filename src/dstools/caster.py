9bd563a74eba9f79792724c715eb70737f49edd2
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-08-06 14:00:42 -0400

    adds strict opt to cat caster

--- src/dstools/caster.py
+++ src/dstools/caster.py
@@ -2,20 +2,28 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class CategoricalCaster(BaseEstimator, TransformerMixin):
-    def __init__(self, cols=None):
+    def __init__(self, cols=None, strict=True):
+        self.strict = strict
         self.cols = cols or []
         self.dtypes = {}
 
     def fit(self, X, y=None):
         for col in self.cols:
-            # cast column
-            X[col] = X[col].astype('category')
-            # save resulting dtype
-            self.dtypes[col] = X[col].dtype
+
+            if col in X:
+                # cast column
+                X[col] = X[col].astype('category')
+                # save resulting dtype
+                self.dtypes[col] = X[col].dtype
+            elif self.strict:
+                raise ValueError('{} was initialized with column "{}", but '
+                                 'it does not appear in the data'.format(
+                                     type(self).__name__, col))
 
         return self
 
     def transform(self, X):
         for col in self.cols:
             X[col] = X[col].astype(self.dtypes[col])
+
         return X
--- tests/test_caster.py
+++ tests/test_caster.py
@@ -1,3 +1,4 @@
+import pytest
 from dstools.caster import CategoricalCaster
 import pandas as pd
 from pandas.core.dtypes.dtypes import CategoricalDtype
@@ -13,3 +14,22 @@ def test_cast_categorical():
     out = caster.transform(test)
 
     assert out.cat.dtype == CategoricalDtype(['a', 'b', 'c'], ordered=False)
+
+
+def test_raise_if_missing_and_strict():
+    train = pd.DataFrame({'cat': ['c', 'b', 'a', 'a', 'a', 'b']})
+    caster = CategoricalCaster(cols=['cat', 'another_cat'])
+
+    with pytest.raises(ValueError) as excinfo:
+        caster.fit(train)
+
+    expected = ('CategoricalCaster was initialized with column "another_cat", '
+                'but it does not appear in the data')
+    assert expected in str(excinfo.value)
+
+
+def test_skip_if_missing_and_not_strict():
+    train = pd.DataFrame({'cat': ['c', 'b', 'a', 'a', 'a', 'b']})
+    caster = CategoricalCaster(cols=['cat', 'another_cat'], strict=False)
+
+    caster.fit(train)
