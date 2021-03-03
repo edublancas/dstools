d068783ea079f8f227d8abdb45930944bdb0f452
Eduardo Blancas Reyes <github@blancas.io>
2021-03-03 09:56:10 -0600

    fix

--- src/dstools/profiling/__init__.py
+++ src/dstools/profiling/__init__.py
@@ -11,9 +11,12 @@ def profile(relation,
             agg=None,
             return_all=False):
     if group_by:
-        return _simple(relation, mappings, alias)
-    else:
         return _agg(relation, mappings, alias, group_by, agg, return_all)
+    else:
+        if not agg:
+            raise ValueError(
+                'agg contain at least one value if passing group_by')
+        return _simple(relation, mappings, alias)
 
 
 def _simple(relation, mappings, alias):
